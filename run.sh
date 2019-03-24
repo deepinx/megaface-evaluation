afe337d2b676203459b97e7e86a6c357e3cd778b
DeepinX <liuxcc@163.com>
2019-03-24 09:13:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,16 +13,3 @@ python -u remove_noises.py --algo "$ALGO" --facescrub-noises "$ROOT/data/facescr
 export LD_LIBRARY_PATH="/usr/local/lib/opencv2.4:$LD_LIBRARY_PATH" 
 python -u devkit/experiments/run_experiment.py "$ROOT/"$ALGO"_feature_out_clean/megaface" "$ROOT/"$ALGO"_feature_out_clean/facescrub" _"$ALGO".bin ./"$ALGO"_results/ -p ../templatelists/facescrub_features_list.json
 
-
-
-# ALGO="mobilefacenet" #ms1mv2
-# ROOT="/media/3T_disk/my_datasets/megaface"
-# echo $ROOT
-# DEVKIT="$ROOT/devkit/experiments"
-
-# python -u gen_megaface.py --gpu 0 --algo "$ALGO" --model './models/model-y1-test2/model,0' --facescrub-lst "$ROOT/data/facescrub_lst" --megaface-lst "$ROOT/data/megaface_lst" --facescrub-root "$ROOT/data/facescrub_images" --megaface-root "$ROOT/data/megaface_images" --output "$ROOT/"$ALGO"_feature_out"
-# python -u remove_noises.py --algo "$ALGO" --facescrub-noises "$ROOT/data/facescrub_noises.txt" --megaface-noises "$ROOT/data/megaface_noises.txt" --facescrub-lst "$ROOT/data/facescrub_lst" --megaface-lst "$ROOT/data/megaface_lst" --feature-dir-input "$ROOT/"$ALGO"_feature_out" --feature-dir-out "$ROOT/"$ALGO"_feature_out_clean"
-
-# cd "$DEVKIT"
-# export LD_LIBRARY_PATH="/usr/local/lib/opencv2.4:$LD_LIBRARY_PATH" 
-# python -u run_experiment.py "$ROOT/"$ALGO"_feature_out_clean/megaface" "$ROOT/"$ALGO"_feature_out_clean/facescrub" _"$ALGO".bin ../../"$ALGO"_results/ -s 1000000 -p ../templatelists/facescrub_features_list.json
